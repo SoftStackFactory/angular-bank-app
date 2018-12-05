dcec32228df8cf5725d3a44d90d4548463080480
Robert Brown <robertlb4@gmail.com>
2018-12-05 14:22:35 -0800

    update app.component.spec to import needed components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { routes } from './app.router';
+import { NavComponent } from './nav/nav.component';
+import { APP_BASE_HREF } from '@angular/common';
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent,
       ],
-    }).compileComponents();
+      imports: [routes],
+      providers: [{provide: APP_BASE_HREF,  useValue: '/'}]
+    })
+    .compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bank-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bank of San Diego');
+    // fixture.detectChanges();
+    // compiled.querySelector('h1').textContent = 'foo'
+    // expect(compiled.querySelector('h1').textContent).toContain('foo');
   }));
+  
 });
--- src/app/atm/atm.component.spec.ts
+++ src/app/atm/atm.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AtmComponent } from './atm.component';
 
 describe('AtmComponent', () => {
