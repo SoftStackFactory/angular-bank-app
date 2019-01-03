4c8975eccd36605b3951bfc96d19157fb8e2a9c2
Robert Brown <robertlb4@gmail.com>
2019-01-03 14:12:48 -0800

    split tests into different suites

--- src/app/bank.service.spec.ts
+++ src/app/bank.service.spec.ts
@@ -13,15 +13,5 @@ describe('BankService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should update account balance when updatebalance method is called', inject([BankService], (service: BankService) => {
-    service.updateBalance(5)
-    expect(service.account.balance).toEqual(5.01)
-  }));
 
-  it('should update the list of transactions when a transaction is complete', inject([BankService], (service: BankService) => {
-    let numOfTransactions = service.account.transactions.length;
-    service.addTransaction('deposit', 1)
-    let numOfNewTransactions = service.account.transactions.length - numOfTransactions;
-    expect(numOfNewTransactions).toEqual(1);
-  }))
 });
